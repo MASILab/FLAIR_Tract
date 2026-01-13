d56097caef8114a5b15d754fe125cb3c23dca1ee
tm-schwartz <tm.sc@protonmail.com>
2026-01-12 19:24:20 -0600

    added mprage to datacode

--- resources/dataset/datacode.py
+++ resources/dataset/datacode.py
@@ -2,9 +2,22 @@ from plumbum import local
 import pathlib as pl
 import pandas as pd
 
+
+def hierarch_pq(grp):
+    if grp.path.str.contains("double").any():
+        ret = grp[grp.path.str.contains("double")]
+    elif grp.path.str.contains("PreQualrun-2").any():
+        ret = grp[grp.path.str.contains("PreQualrun-2")]
+    elif grp.path.str.contains("HARDI").any():
+        ret = grp[grp.path.str.contains("HARDI")]
+    else:
+        ret = grp[grp.path.str.endswith("PreQual")]
+    return ret
+
+
 rg = local["rg"]
 prequal_list = "/nfs2/harmonization/BIDS/ALL_PREQUALS_11_20_25.txt"
-pattern = "(NACC|BIOCARD|OASIS4|WRAP|BLSA|WASHU|HABSHD)"
+pattern = r"(\bNACC\b|BIOCARD|WRAP\b|BLSA|WASHU|HABSHD)"
 prequal_dirs = list(
     filter(
         lambda s: pl.Path(s).is_dir() & pl.Path(s).exists(),
@@ -26,28 +39,65 @@ sub_sess_count.name = "sub_sess_count"
 df = df.merge(sub_sess_count, on="subject")
 df_sub_gr1 = df[df.sub_sess_count > 1]
 fdfind = local["fdfind"]
-flair_list = fdfind(
-    "FLAIR.nii.gz",
-    "--base-directory=/nfs2/harmonization/BIDS",
-    "--search-path=BIOCARD",
-    "--search-path=WASHU",
-    "--search-path=HABSHD",
-    "--search-path=WRAP",
-    "--search-path=NACC",
-    "--search-path=OASIS4",
-    "--search-path=BLSA",
+if not pl.Path("FLAIR_LIST_01-12-2026.csv").exists():
+    flair_list = fdfind(
+        "FLAIR.nii.gz",
+        "--base-directory=/nfs2/harmonization/BIDS",
+        "--search-path=BIOCARD",
+        "--search-path=WASHU",
+        "--search-path=HABSHD",
+        "--search-path=WRAP",
+        "--search-path=NACC",
+        "--search-path=BLSA",
+        "--exclude=WRAPnew$",
+        "--exclude=SCAN$",
+        "--exclude=dwi",
+        "--exclude=func",
+        "--exclude=perf",
+        "--absolute-path",
+    )
+    flair_list = flair_list.split()
+    flair_ser = pd.Series(flair_list, name="flair")
+    flair_df = flair_ser.to_frame()
+    flair_df.to_csv("FLAIR_LIST_01-12-2026.csv")
+else:
+    flair_df = pd.read_csv("FLAIR_LIST_01-12-2026.csv")
+flair_df["subject"] = flair_df.flair.map(lambda s: s.split("/")[5])
+flair_df["session"] = flair_df.flair.map(lambda s: s.split("/")[6])
+dedup_flair_df = flair_df[["subject", "flair"]].drop_duplicates(
+    subset="subject"
 )
-flair_list = flair_list.split()
-flair_ser = pd.Series(flair_list, name="flair")
-flair_df = flair_ser.to_frame()
-flair_df["subject"] = flair_df.flair.map(lambda s: s.split("/")[1])
-flair_df["session"] = flair_df.flair.map(lambda s: s.split("/")[2])
-df_flair_subsess = df_sub_gr1.merge(flair_df, on=["subject", "session"], how='left')  # left because dont want to drop if no flair->session
 
-# Drop oasis because only 11 unique subs with >1 prequal
+mprage = []
+for row in df.itertuples():
+    p = list(pl.Path(row.path).parent.parts)
+    _ = p.pop(5)
+    pth = pl.Path("/").joinpath(*p[0:])
+    res: list[str] = fdfind(
+        r"(MPRAGE|T1).*nii.gz", f"--base-directory={pth}", "--absolute-path"
+    ).split()
+    mprage.extend(res)
 
-df_flair_subsess[df_flair_subsess.dataset != 'OASIS4'].groupby('dataset').subject.sample(25)
-subject_list = df_flair_subsess[df_flair_subsess.dataset != 'OASIS4'].groupby('dataset').subject.sample(25)
-df_flair_subsess[df_flair_subsess.subject.isin(subject_list)]
-df_flair_subsess[df_flair_subsess.subject.isin(subject_list)].to_csv("UPDATED_DATASET_12-29-2025.csv", index=False)
+t1_df = pd.Series(mprage, name="mprage").to_frame()
+t1_df["subject"] = t1_df.mprage.map(lambda s: s.split("/")[5])
+t1_df = t1_df.drop_duplicates(subset="subject")
 
+df_flair_subsess = df_sub_gr1.merge(dedup_flair_df, on=["subject"], how="left")
+df_flair_t1 = df_flair_subsess.merge(t1_df, on=["subject"], how="left")
+
+subject_list = (
+    df_flair_t1[
+        ~(df_flair_t1.dataset.isin(("OASIS4", "WRAPnew", "SCAN")))
+        & (df_flair_t1.flair.notna())
+    ]
+    .groupby("dataset")
+    .subject.sample(25)
+)
+df_flair_t1[df_flair_t1.subject.isin(subject_list)].to_csv(
+    "UPDATED_DATASET_01-12-2026.csv", index=False
+)
+df_flair_t1.groupby(by=["subject", "session"]).apply(
+    hierarch_pq, include_groups=False
+).reset_index().sort_values(by=["dataset", "subject", "session"]).drop(
+    columns="level_2"
+k.groupby(by="subject").apply(lambda grp: grp.iloc[:2, :], include_groups=False)
